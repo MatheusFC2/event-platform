7fa692543aa20438e364322414f0da8093228e16
MatheusFC2 <matheusfreitascampos@gmail.com>
2022-06-27 15:10:00 -0300

    feat: added .env

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({ 
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ob1rxv1orz01xi26h531h2/master',
+    uri: import.meta.env.BASE_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}` 
+    },
     cache: new InMemoryCache
 })
